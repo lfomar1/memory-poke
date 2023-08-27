2b84caa5117cb2e1446997d2e0d6b8bf933fe333
Lucas <107668539+lfomar1@users.noreply.github.com>
2023-08-27 03:31:13 -0400

    Basic logic done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
 import "./App.css";
-
+import { useFetch } from "./components/useFetch";
 function App() {
-  return;
+  const { data } = useFetch(
+    "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0."
+  );
+  const imgHandler = (url) => {
+    console.log(url);
+  };
+  return (
+    <div>
+      <ul>
+        {data?.results.map((pokemon) => (
+          <li key={pokemon.url}>
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${getPokemonIdFromUrl(
+                pokemon.url
+              )}.png`}
+              alt={pokemon.name}
+              onClick={() => imgHandler(pokemon.url)}
+            />
+            {pokemon.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
+function getPokemonIdFromUrl(url) {
+  const urlParts = url.split("/");
+  return urlParts[urlParts.length - 2];
+}
 export default App;
---
+++ src/components/useFetch.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(true);
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .finally(() => setLoading(false));
+  }, []);
+  return { data, loading };
+};
